411d9d0df99a5035b78e9426c9070bd758258dd2
Willian <willianrssi@gmail.com>
2018-01-05 21:54:40 -0200

    propTypes sample

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Button from './button'
 
 class App extends Component {
   render () {
     return (
       <div className='container' >
-        My App
-    </div>
+        <Button handleClick={() =>
+          console.log('clicou')
+          }>
+          Clica em mim
+        </Button>
+      </div>
     )
   }
 }
