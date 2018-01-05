f3a287eb7d22be60031700bf80b92ef753944dc3
Willian <willianrssi@gmail.com>
2018-01-05 15:27:56 -0200

    lessons about state

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Square from './square'
+import Button from './button'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      color: 'black'
+    }
+  }
   render () {
     return (
       <div className='container' >
-        My App
-    </div>
+        <Square color={this.state.color} />
+
+        {['red', 'green', 'blue'].map((color) => (
+          <Button
+            key={color}
+            handleClick={() => this.setState({ color })}>
+            {color}
+          </Button>
+        )
+        )}
+      </div>
     )
   }
 }
