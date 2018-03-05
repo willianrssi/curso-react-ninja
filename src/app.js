dbb93cf0fdf14ca0c547b44b97eeb403535139dd
Willian <willianrssi@gmail.com>
2018-03-05 15:33:27 -0300

    Updated with configuration of jest snapshots tests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Title from 'components/title'
 
 class App extends Component {
   render () {
     return (
-      <div> App </div>
+      <div><Title>My App</Title></div>
     )
   }
 }
